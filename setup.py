ffb713b420586c01f93e7392fedda0fc962daf4f
Chandan Kumar <chandankumarsingh1351@gmail.com>
2024-08-19 17:03:29 +0530

    setup completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version= '0.0.0',
+    author="Chandan Kumar",
+    author_email="chandankumarsingh1351@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
